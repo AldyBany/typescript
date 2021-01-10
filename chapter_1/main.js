fb205ec11e5e37d084546b84be2dcc226ed3d502
Aldy Bany <64736339+AldyBany@users.noreply.github.com>
2021-01-10 22:41:47 +0200

    update

--- chapter_1/main.js
+++ chapter_1/main.js
@@ -3,7 +3,7 @@ let array = [1,2,3,4,5];
 const result = array.reduce((ac,val)=>{
   return ac + val;
 });
-console.log(result);
+// console.log(result);
 let fruits = ["apple","mango","orange","lemon","kiwi"];
 // console.log(fruits.indexOf("orange"))
 // console.log(fruits.includes("lemon"))
@@ -22,5 +22,40 @@ const student = students.find(s =>{
   return s.age == 18;
 })
 
-console.log(student);
+// console.log(student);
+
+// document.getElementById("name")
+// console.log("Program starts")
+
+// setTimeout(()=>{
+//     document.getElementById("name").innerText = "Hi aldy"
+// },3000)
+
+// let i = 0;
+// let id = setInterval(()=>{
+//     document.getElementById('name').innerText = ++ i
+//     if(i ==50){
+//         clearInterval(id)
+//     }
+// },100)
+
+
+// let i =100;
+// let j= 2;
+// try{
+//     if(j ==0){
+//         throw "j cannot be zero"
+//     }
+
+// }catch(err){
+//     console.log(err)
+// }
+
+var temp = document.querySelectorAll("button")
+temp[1].innerText = "Hi gaick"
+
+
+
+
+
 
